5b4e8b76765db795cc5e0d2944854c738b250c5b
Ibrahim Jarrah <70149572+ibrahimjarrah@users.noreply.github.com>
2022-07-29 23:16:33 +0300

    arraySorting
    In this version both positive and negative numbers are being sorted.

---
+++ arraySorting.js
@@ -0,0 +1,9 @@
+let numbers = [11, 2, 37, 17, -4801,5100, -117.987, 98];
+
+function sortNumber(a,b){
+   return a - b;
+}
+
+ let finalSorted=numbers.sort(sortNumber) ;
+
+ console.log(finalSorted);
